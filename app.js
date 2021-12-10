68a709f33989b2adc417cdb7b78ce77d23177dbb
Art Schedrov <a.sch@tutanota.com>
2021-12-11 03:00:51 +0700

    edit env file

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const authRoutes = require('./routes/auth');
 const collectionsRoutes = require('./routes/collections');
 const statisticsRoutes = require('./routes/statistics');
-const keys = require('./config/keys');
 
 const mongoose = require('mongoose');
 
-mongoose.connect(keys.mongoURI)
+mongoose.connect(process.env.DB_URL)
   .then(() => console.log('Mongo connected'))
   .catch(error => console.log(error));
 
