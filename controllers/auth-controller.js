6637e39a743ca31e0f5b68f67eb70aa517772b82
Art Schedrov <a.sch@tutanota.com>
2021-12-11 18:10:30 +0700

    add default role for user

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -41,7 +41,8 @@ module.exports.register = async function(req, res) {
     const password = req.body.password;
     const user = new User({
       email: req.body.email,
-      password: bcrypt.hashSync(password, salt)
+      password: bcrypt.hashSync(password, salt),
+      role: req.body.role
     });
 
     try {
--- routes/collections.js
+++ routes/collections.js
@@ -5,15 +5,15 @@ const upload = require('../middleware/upload');
 const collectionController = require('../controllers/collection-controller');
 const scribbleController = require('../controllers/scribble-controller');
 
-router.get('/', collectionController.getAllCollections);
-router.get('/:id', collectionController.getCollectionById);
-router.post('/', collectionController.createCollection);
-router.patch('/:id', collectionController.updateCollectionById);
-router.delete('/:id', collectionController.deleteCollectionById);
+router.get('/', passport.authenticate('jwt', {session: false}), collectionController.getAllCollections);
+router.get('/:id', passport.authenticate('jwt', {session: false}), collectionController.getCollectionById);
+router.post('/', passport.authenticate('jwt', {session: false}), collectionController.createCollection);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), collectionController.updateCollectionById);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), collectionController.deleteCollectionById);
 
-router.post('/scribble/create', upload.single('image'), scribbleController.createScribble);
-router.get('/scribble/:id', scribbleController.getScribbleById);
-router.patch('/scribble/:id', scribbleController.updateScribbleById);
-router.delete('/scribble/:id', scribbleController.deleteScribbleById);
+router.post('/scribble/create', upload.single('image'), passport.authenticate('jwt', {session: false}), scribbleController.createScribble);
+router.get('/scribble/:id', passport.authenticate('jwt', {session: false}), scribbleController.getScribbleById);
+router.patch('/scribble/:id', passport.authenticate('jwt', {session: false}), scribbleController.updateScribbleById);
+router.delete('/scribble/:id', passport.authenticate('jwt', {session: false}), scribbleController.deleteScribbleById);
 
 module.exports = router;
