3c0b0ec05e96ab93bdb3d814f9535e946310404e
Art Schedrov <a.sch@tutanota.com>
2021-12-31 17:37:38 +0700

    add routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './user/login-page/login-page.component';
+import { RegisterPageComponent } from './user/register-page/register-page.component';
+import { AuthLayoutComponent } from './user/shared/auth-layout/auth-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AuthLayoutComponent, children: [
+      {path: '', redirectTo: '/login', pathMatch: 'full'},
+      {path: 'login', component: LoginPageComponent},
+      {path: 'register', component: RegisterPageComponent}
+    ]
+  },
+  {
+    path: '', canActivate: [], loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
