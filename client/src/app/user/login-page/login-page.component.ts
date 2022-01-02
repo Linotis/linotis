9b1da00de36e5388bfc6d215dbd193921a0edac3
Art Schedrov <a.sch@tutanota.com>
2022-01-02 18:33:16 +0700

    add login

--- client/src/app/user/login-page/login-page.component.ts
+++ client/src/app/user/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+  loginSubscription!: Subscription;
+
+  constructor(private auth: AuthService) {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
   }
 
+  ngOnDestroy() {
+    if(this.loginSubscription) {
+      this.loginSubscription.unsubscribe();
+    }
+  }
+
+  onSubmit(): void {
+    this.form.disable();
+    this.loginSubscription = this.auth.login(this.form.value).subscribe(
+      () => console.log("Login ok"), error => {console.warn(error), this.form.enable()}
+    )
+  }
 }
---
+++ client/src/app/user/shared/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { IUserLogin } from '../interfaces/user.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private token = '';
+
+  constructor(private http: HttpClient) {}
+
+  register() {}
+
+  login(user: IUserLogin): Observable<{token: string}> {
+    return this.http.post<{token: string}>('/api/auth/login', user).pipe(tap(({token}) => {
+      localStorage.setItem('auth-token', token);
+    }))
+  }
+
+  setToken(token: string) {
+    this.token = token;
+  }
+
+  getToken(): string {
+    return this.token;
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.token;
+  }
+
+  logOut() {
+    this.setToken('');
+    localStorage.clear();
+  }
+}
