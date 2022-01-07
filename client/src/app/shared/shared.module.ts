1e0e031e72b9c7e2358dcf3c3bd0fd74587c8f5b
Art Schedrov <a.sch@tutanota.com>
2022-01-07 07:42:49 +0700

    styling 400 page

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ErrorPageComponent } from './component/error-page/error-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ErrorPageComponent],
   imports: [
     CommonModule,
     FormsModule,
