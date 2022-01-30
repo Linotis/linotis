9f2a61f984ee0f45faf609e332232d262c1707f8
Art Schedrov <a.sch@tutanota.com>
2022-01-30 19:16:07 +0700

    add tests

--- client/src/app/data/service/language/language.service.spec.ts
+++ client/src/app/data/service/language/language.service.spec.ts
@@ -1,16 +1,61 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { LanguageService } from './language.service';
 
+const expectedUrlGetLang = '/api/languages';
+
 describe('LanguageService', () => {
   let service: LanguageService;
+  let controller: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [LanguageService]
+    });
     service = TestBed.inject(LanguageService);
+    controller = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should getLanguages return languages', () => {
+    let languages = [
+      {
+        _id: '1',
+        name: 'test',
+        icon: 'test'
+      },
+      {
+        _id: '2',
+        name: 'test1',
+        icon: 'test1'
+      }
+    ];
+
+    let actualLanguages: any;
+
+    service.getLanguages().subscribe(result => actualLanguages = result);
+
+    const request = controller.expectOne(expectedUrlGetLang);
+
+    request.flush([
+        {
+          _id: '1',
+          name: 'test',
+          icon: 'test'
+        },
+        {
+          _id: '2',
+          name: 'test1',
+          icon: 'test1'
+        }
+      ]);
+
+    controller.verify();
+    expect(actualLanguages).toEqual(languages);
   });
 });
